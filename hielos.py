36f72a17bc1490d33fb9840b06eebeb8e1f89b80
Marianitasss <94995113+Marianitasss@users.noreply.github.com>
2023-01-30 23:42:53 -0300

    Add files via upload

--- hielos.py
+++ hielos.py
@@ -4,12 +4,15 @@ Created on Fri Jan 27 16:03:30 2023
 
 @author: Mariam
 """
+import pandas as pd
 import xarray as xr
-hielos=xr.open_dataset('C:/Users/Mariam/Documents/HIELOS/NSIDC0081_SEAICE_PS_S25km_20220101_v2.0.nc')
+hielos=xr.open_dataset('C:/Users/Mariam/Documents/HIELOS/NSIDC0081_SEAICE_PS_S25km_20230101_v2.0.nc')
 
 
 hielos.values
 hielos.data_vars
+hielos.variables.keys()
+hielos.variables.values()
 hielos.variables["F16_ICECON"]
 hielos.variables["crs"]
 hielos.coords
@@ -17,5 +20,6 @@ hielos["F16_ICECON"]
 hielos.attrs
 hielos.F16_ICECON
 a=hielos['x'].to_index()
-df = hielos.to_dataframe()
-df = hielos.to_dataframe()
+satel_todos = hielos.to_dataframe()
+satelite16 =satel_todos["F16_ICECON"]
+satel_todos.to_csv('satelites.csv',sep='\t')
